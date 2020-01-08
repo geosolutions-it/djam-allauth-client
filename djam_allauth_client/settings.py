831df74e7d393c226ddc4fbc668860dd054c019c
Piotr Dankowski <dankowski.revolve@gmail.com>
2020-01-08 18:07:21 +0100

    fix type

--- djam_allauth_client/settings.py
+++ djam_allauth_client/settings.py
@@ -1,5 +1,5 @@
 DJAM_DOMAIN = 'localhost:8800'
 DJAM_DOMAIN_SCHEMA = 'http'
-DJAM_OPENID_PREFIX = 'oprnid'
+DJAM_OPENID_PREFIX = 'openid'
 DJAM_PROVIDER_NAME = 'Mapstand signin service'
 #'%s/djamauthprovider/*' % FORCE_SCRIPT_NAME
