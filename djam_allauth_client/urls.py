159ee3ea72489cef89449d0554773faf346ed761
Piotr Dankowski <dankowski.revolve@gmail.com>
2020-01-08 18:05:04 +0100

    add missing import

--- djam_allauth_client/urls.py
+++ djam_allauth_client/urls.py
@@ -1,4 +1,5 @@
 from allauth.socialaccount.providers.oauth2.urls import default_urlpatterns
+from djam_allauth_client.views import djam_logout
 from django.conf.urls import url
 
 from djam_allauth_client.providers import DjamProvider
